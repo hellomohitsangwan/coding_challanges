bcf0407dbbeab826502a1c21648e884567cd2bcd
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-03-31 15:23:21 +0530

    efficient soln. added

--- codeforces/petya_and_strings.cpp
+++ codeforces/petya_and_strings.cpp
@@ -61,6 +61,25 @@ void solve() {
         // stcout << *first;  
     }
 }
+void solveEfficient() {
+    string s1 , s2; cin>>s1>>s2;
+
+    //transform
+    for (int i = 0; i < s1.length(); i++) {
+        if(s1[i]>=97) s1[i]=s1[i]-32;
+        if(s2[i]>=97) s2[i]=s2[i]-32;
+    }
+    for (int i = 0; i < s1.length(); i++) {
+        if(s1[i] < s2[i]) {
+            cout<<"-1";
+            return;
+        } else if(s1[i] > s2[i]) {
+            cout<<"1";
+            return;
+        }
+    }
+    cout<<"0";
+}
 int main()  {
-    solve();
+    solveEfficient();
 }
